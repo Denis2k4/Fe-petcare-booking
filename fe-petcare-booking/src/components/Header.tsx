c9618e0ee8ac96e1cf4baeae1129e79565896110
Nguyễn Quang Anh <154863682+NguyenQuangAnh04@users.noreply.github.com>
2025-08-14 17:55:04 +0700

    1

---
+++ fe-petcare-booking/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='flex flex-1/2 items-center '>
+            1122
+        </div>
+    );
+};
+
+export default Header;
